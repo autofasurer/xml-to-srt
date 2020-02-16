4a19bd88ed8e5525501eb06b720bf54f0f61ed45
Visualantics Interactive <brecht@visualantics.net>
2020-02-16 22:34:48 +0100

    -snake_case for function and vars -directly 0-padding numbers in string formatting -splitting file extension on .

--- xml-to-srt.py
+++ xml-to-srt.py
@@ -2,13 +2,13 @@ import xml.etree.ElementTree as ET
 import os, argparse
 
 
-def stringToTimecode(tc):
+def string_to_timecode(tc):
     #convert the int tc into hours, minutes, seconds and frames
     frames = (tc%25)*40 #.srt files take frames in milliseconds, hence the *40
     seconds = ((tc-(tc%25))//25)%60
     minutes = ((tc-(tc%25))//25)//60
     hours = seconds//3600
-    return f'{str(hours).zfill(2)}:{str(minutes).zfill(2)}:{str(seconds).zfill(2)},{str(frames).zfill(3)}'
+    return f'{hours:02d}:{minutes:02d}:{seconds:02d},{frames:03d}'
 
 def main():
     #setup the argument parser, add the requred filename and set the var file to the path/filename provided
@@ -17,14 +17,14 @@ def main():
     args = vars(ap.parse_args())
     file = args["filename"]
 
-    filename = os.path.basename(file)[:-4] #get the name of the file without location or TLA filetype - used to name the .srt file lateron
-    filenamePath = os.path.dirname(file) #the absolute path of the input file, .srt will be created in the same location
+    filename, _ = os.path.basename(file).rsplit('.', 1) #get the name of the file without location or TLA filetype - used to name the .srt file lateron
+    filename_path = os.path.dirname(file) #the absolute path of the input file, .srt will be created in the same location
 
     tree = ET.parse(file) #parse the xml file
     root = tree.getroot()
     sub = root.findall('.//clipitem/filter/')
-    start = root.findall('.//clipitem/start')
-    end = root.findall('.//clipitem/end')
+    start = root.findall('.//clipitem/start') #start time of subtitle in frames
+    end = root.findall('.//clipitem/end') #end time of subtitle in frames
 
 #Filter out the elements <name> which are empty or are part of another type of <filter>
     subtitles = []
@@ -33,10 +33,10 @@ def main():
             if subs[0].text is not None and len(subs[0].text) > 1:
                 subtitles.append(subs[0].text)
 
-    with open(f"{filenamePath}/{filename}.srt", 'w+') as export:
+    with open(f"{filename_path}/{filename}.srt", 'w+') as export:
         num = 1
         for i, sub in enumerate(subtitles):
-            export.write(f'{num}\n{stringToTimecode(int(start[i].text))} --> {stringToTimecode(int(end[i].text))}\n{sub}\n\n')
+            export.write(f'{num}\n{string_to_timecode(int(start[i].text))} --> {string_to_timecode(int(end[i].text))}\n{sub}\n\n')
             num += 1
 
 
